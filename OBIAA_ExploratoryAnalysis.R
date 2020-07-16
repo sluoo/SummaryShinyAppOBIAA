f9b807528d394c8e6c049424912005dfbf410c3d
Sherry Luo <sherryluo08@gmail.com>
2020-07-16 16:54:24 -0400

    Update

--- OBIAA_ExploratoryAnalysis.R
+++ OBIAA_ExploratoryAnalysis.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(tibble)
+library(plotly)
 
 #Goal: Create shiny dashboards based on industry/space usage/staffing/revenue
 
@@ -12,12 +13,20 @@ dta <- DTG_Approved_Grants_Sorted_by_Date
 #Select necessary columns for analysis
 #Remove denied funding cases 
 dta1 <- (dta %>% 
-           select(`Business Name`,Industry, BIA, City, Municipality, Currency, `Matching Funds`, `Annual Revenue`,
-                  `Square Feet`, Staff, Status,`How Did You Hear About the Grant`,`Digital Marketing $`,
-                  Website, Software, `Digital Training`, Hardware,DSS, Region)
-         %>% rename(ReferalGrant = `How Did You Hear About the Grant`,
+           select(`Business Name`,Industry, BIA, City, Municipality,  Currency, `Matching Funds`, 
+                  `Annual Revenue`,`Square Feet`, Staff, Status,`Digital Marketing $`,
+                  Website, Software, `Digital Training`, Hardware, DSS,Region)
+         %>% mutate(Currency = gsub("[\\$,]","",Currency))
+         %>% rename(
                     DigitalMarket = `Digital Marketing $`,
-                    DigitalTrain = `Digital Training`)
+                    DigitalTrain = `Digital Training`,
+                    AnnualRevenue = `Annual Revenue`,
+                    SquareFt = `Square Feet`)
+         %>% mutate(DigitalMarket = as.numeric(gsub("[\\$,]","",DigitalMarket)),
+                    DigitalTrain = as.numeric(gsub("[\\$,]","",DigitalTrain)),
+                    Website = as.numeric(gsub("[\\$,]","",Website)),
+                    Software = as.numeric(gsub("[\\$,]","",Software)),
+                    Hardware = as.numeric(gsub("[\\$,]","",Hardware)))
          %>% filter(Status != "Denied"))
 
 #Add 0 to NA for funding columns 
@@ -27,33 +36,64 @@ dta1$Software[c(which(is.na(dta1$Software==TRUE)))] <-0
 dta1$DigitalTrain[c(which(is.na(dta1$DigitalTrain==TRUE)))] <-0
 dta1$Hardware[c(which(is.na(dta1$Hardware==TRUE)))] <-0
 
+
 #Check N/A 
 tbl <- is.na.data.frame(dta1)
 sumNA <- apply(tbl,2,sum)
 print(sumNA)
 
-View(select(dta1,`Business Name`,Industry)
-     %>% filter(is.na(Industry)))
+###Question 1###
 
-#Predict staff for lacroix 
+##How are funds potentially distributed/proportion for each area? 
+totalAmount <- sum(dta1$Currency)
+totalRows <- nrow(dta1)
 
-#Possibly subcategorized food & beverage?? 
-FoodBevTbl <- (dta1
-              %>% filter(Industry == "Food & Beverage" | Industry == "Restaurant"))
+#Number of businesses who requested funding for each area 
+nDM = length(which(dta1$DigitalMarket != 0))
+sumDM = sum(dta1$DigitalMarket)
 
-RestTbl <- (dta1
-            %>% filter(Industry=="Restaurant"))
+nDT = length(which(dta1$DigitalTrain != 0))
+sumDT = sum(dta1$DigitalTrain)
 
-RetailTbl <- (dta1
-            %>% filter(Industry=="Retail"))
+nWeb = length(which(dta1$Website != 0))
+sumWeb = sum(dta1$Website)
 
-write_csv(FoodBevTbl,path="FoodBevTbl.csv")
+nSoft = length(which(dta1$Software != 0))
+sumSoft = sum(dta1$Software)
+
+nHard = length(which(dta1$Hardware != 0))
+sumHard = sum(dta1$Hardware)
+
+all.n = c(nDM,nDT,nWeb,nSoft,nHard)
+prop = all.n / sum(all.n)
+ymax1 = cumsum(prop)
+ymin1 = c(0,ymax1[1:4])
+sumCol = c(sumDM,sumDT,sumWeb,sumSoft,sumHard)
 
 
+data.donut <- data.frame(
+  Category = c("Digital Marketing", "Digital Training", "Website",
+               "Software", "Hardware"),
+  prop = prop,
+  ymax = ymax1, 
+  ymin = ymin1,
+  sum1 = sumCol
+)
 
+#Compute label/position
+data.donut$label <- paste0(data.donut$Category, "\n Proportion:", round(data.donut$prop,3), 
+                           "\n Total Funding:", "$",data.donut$sum1)/10000)
 
+data.donut$labelPosition <- (data.donut$ymax + data.donut$ymin) / 2
 
+g <- (ggplot(data.donut, aes(ymax=ymax,ymin=ymin,xmax=4,xmin=3,fill=Category))
+       + geom_rect(show.legend = FALSE)
+       + geom_label( x=3.5, aes(y=labelPosition, label=label), size=3) 
+       + coord_polar(theta="y")
+       + theme_void()
+       + scale_fill_brewer(palette = 1))
 
+print(g)
 
 
 
